db6effc98db7c821a5ced43e8ef540180d4beac4
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-10-16 23:05:20 +0530

    Add code in developer branch

---
+++ JenkinsMultibranchDemo/JenkinsMultibranchDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsMultibranchDemo
+//
+//  Created by Mantosh Kumar on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
