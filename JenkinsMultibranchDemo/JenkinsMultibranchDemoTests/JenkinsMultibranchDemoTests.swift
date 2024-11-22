e9ad8c79faf3aab45c7326c857472a46e28c15b0
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-11-22 23:07:05 +0530

    Jenkins new project

---
+++ JenkinsMultibranchDemo/JenkinsMultibranchDemoTests/JenkinsMultibranchDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsMultibranchDemoTests.swift
+//  JenkinsMultibranchDemoTests
+//
+//  Created by Mantosh Kumar on 22/11/24.
+//
+
+import Testing
+@testable import JenkinsMultibranchDemo
+
+struct JenkinsMultibranchDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
